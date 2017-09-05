6d296ea853b2cc2363018a0facb6758c3ba5bd7b
Ronit Sajeey <rsajeey@hotmail.com>
2017-09-05 21:29:22 +0100

    debugging removed

--- testing123.sh
+++ testing123.sh
@@ -9,17 +9,17 @@ java -jar Minecraft.jar && java -jar forge-1.12.1-14.22.0.2469-installer.jar && 
 cd ~ && cd .minecraft && rm -d server-resource-packs launcher.jar launcher.pack.lzma launcher_profiles.json
 
 cd ~ && cd .minecraft/libraries/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209
-wget -d https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209-natives-windows.jar
+wget -q https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209-natives-windows.jar
 
 cd ~ && cd .minecraft/libraries/net/java/jinput/jinput-platform/2.0.5
-wget -d https://libraries.minecraft.net/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar
+wget -q https://libraries.minecraft.net/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar
 
 cd ~ && cd .minecraft/libraries/com/mojang/text2speech/1.10.3
-wget -d https://libraries.minecraft.net/com/mojang/text2speech/1.10.3/text2speech-1.10.3-natives-windows.jar
+wget -q https://libraries.minecraft.net/com/mojang/text2speech/1.10.3/text2speech-1.10.3-natives-windows.jar
 
 #cd ~ && cd .minecraft/libraries && mkdir -p ca/weblite/java-objc-bridge/1.0.0 && cd ca/weblite/java-objc-bridge/1.0.0
-#wget -d https://libraries.minecraft.net/ca/weblite/java-objc-bridge/1.0.0/java-objc-bridge-1.0.0.jar
-#wget -d https://libraries.minecraft.net/ca/weblite/java-objc-bridge/1.0.0/java-objc-bridge-1.0.0-natives-osx.jar
+#wget -q https://libraries.minecraft.net/ca/weblite/java-objc-bridge/1.0.0/java-objc-bridge-1.0.0.jar
+#wget -q https://libraries.minecraft.net/ca/weblite/java-objc-bridge/1.0.0/java-objc-bridge-1.0.0-natives-osx.jar
 cd ~
 
 mv .minecraft Documents && mv Downloads/launcher.jar Documents && cd Documents && zip -q -9 100d2d030f50319275c76e9ec8d03771 .
