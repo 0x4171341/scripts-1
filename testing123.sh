ba1c5d3a9476d3f5313d18ff7a7b96bd24987868
Ronit Sajeey <rsajeey@hotmail.com>
2017-09-05 17:11:14 +0100

    A minor change

--- testing123.sh
+++ testing123.sh
@@ -1,6 +1,6 @@
 sudo apt-get update && sudo apt-get install openjdk-8-jre-headless -y
 
-wget s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar &&
+cd Downloads && wget s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar &&
 wget files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.1-14.22.0.2469/forge-1.12.1-14.22.0.2469-installer.jar &&
 wget launcher.mojang.com/mc/launcher/jar/66d552b9ce72dc495a2ec8ef2dcfb7a1d407752c/launcher.jar.lzma
 
