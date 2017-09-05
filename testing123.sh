5e43a5a412f7a46a4dd3c58502e536c04d1b5dc6
Ronit Sajeey <rsajeey@hotmail.com>
2017-09-05 18:52:12 +0100

    some fixes

--- testing123.sh
+++ testing123.sh
@@ -3,8 +3,10 @@ sudo apt-get update && sudo apt-get install openjdk-8-jre-headless -y
 cd Downloads && wget s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar &&
 wget files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.1-14.22.0.2469/forge-1.12.1-14.22.0.2469-installer.jar &&
 wget launcher.mojang.com/mc/launcher/jar/66d552b9ce72dc495a2ec8ef2dcfb7a1d407752c/launcher.jar.lzma &&
-java -jar Minecraft.jar && java -jar forge-1.12.1-14.22.0.2469-installer.jar && java -jar Minecraft.jar &&
-rm forge-1.12.1-14.22.0.2469-installer.jar.log && rm -r server-resource-packs launcher.jar launcher.pack.lzma launcher_profiles.json
+java -jar Minecraft.jar && java -jar forge-1.12.1-14.22.0.2469-installer.jar && rm forge-1.12.1-14.22.0.2469-installer.jar.log &&
+java -jar Minecraft.jar
+
+cd ~ && cd .minecraft && && rm -r server-resource-packs launcher.jar launcher.pack.lzma launcher_profiles.json
 
 # org.lwjgl.lwjgl:lwjgl-platform:2.9.4-nightly-20150209
 cd ~ && cd .minecraft/libraries/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209 &&
