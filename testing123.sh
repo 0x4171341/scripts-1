0ec57554dcd22c40d6cbbe7cae205903eca04fac
Ronit Sajeey <rsajeey@hotmail.com>
2017-09-05 20:18:52 +0100

    hide apt-get messages

--- testing123.sh
+++ testing123.sh
@@ -1,12 +1,10 @@
-sudo apt-get update && sudo apt-get install openjdk-8-jre-headless -y
+sudo apt update -q && sudo apt install -q -y openjdk-8-jre-headless
 
 cd Downloads
 wget -q s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
 wget -q files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.1-14.22.0.2469/forge-1.12.1-14.22.0.2469-installer.jar
 wget -q launcher.mojang.com/mc/launcher/jar/66d552b9ce72dc495a2ec8ef2dcfb7a1d407752c/launcher.jar.lzma && lzma -d launcher.jar.lzma
 java -jar Minecraft.jar && java -jar forge-1.12.1-14.22.0.2469-installer.jar && java -jar Minecraft.jar
-
-rm forge-1.12.1-14.22.0.2469-installer.jar.log
 
 cd ~ && cd .minecraft && rm -d server-resource-packs launcher.jar launcher.pack.lzma launcher_profiles.json
 
